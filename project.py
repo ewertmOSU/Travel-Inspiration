634a77528c1b7dfd72f948ed8733f465a59098d1
ewertmOSU <130020266+ewertmOSU@users.noreply.github.com>
2023-04-24 18:31:12 -0700

    Add files via upload
    Milestone 1 implementation, create UI

---
+++ project.py
@@ -0,0 +1,293 @@
+import sys
+
+while True:
+    print('\n \n')
+    print('Travel Inspriration App \n')
+    print('Find popular travel destinations around the world')
+    print('Please make a selection from the geographical areas listed below \n')
+    print('1) North America')
+    print('2) South America')
+    print('3) Europe \n')
+    print('Enter the number of your selection and press return, or enter 4 to exit')
+    geo = input('Selection:')
+
+    while True: 
+        if geo == '1':
+        # if choice is 1)
+            print('\n')
+            print('North America\'s Top travel destinations')
+            print('Destination 1:')
+            print('Destination 2:')
+            print('Destination 3:')
+            print('Destination 4:')
+            print('Destination 5: \n')
+            print('Enter the number of your selection and press return or enter 6 to choose a new continent.')
+            dest = input('Selection:')
+            if dest == '1':
+            # if choice is destination 1
+                print('\n')
+                print('Destination 1 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 1 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+                
+
+            elif dest == '2':
+            # if choice is destination 2
+                print('\n')
+                print('Destination 2 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 2 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '3':
+            # if choice is destination 3
+                print('\n')
+                print('Destination 3 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 3 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '4':
+            # if choice is destination 4
+                print('\n')
+                print('Destination 4 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 4 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '5':
+            # if choice is destination 5
+                print('\n')
+                print('Destination 5 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 5 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '6':
+                break
+
+            else:
+                if dest not in {'1','2','3','4','5','6'}:
+                    print("\n Invalid input, try again")
+                    continue
+
+        elif geo == '2':
+        # if choice is 2)
+            # Add conjuction
+            print('\n')
+            print('South America\'s Top travel destinations')
+            print('Destination 1:')
+            print('Destination 2:')
+            print('Destination 3:')
+            print('Destination 4:')
+            print('Destination 5: \n')
+            print('Enter the number of your selection and press return, or enter 6 to choose a new continent.')
+            dest = input('Selection:')
+            if dest == '1':
+            # if choice is destination 1
+                print('\n')
+                print('Destination 1 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 1 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '2':
+            # if choice is destination 2
+                print('\n')
+                print('Destination 2 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 2 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '3':
+            # if choice is destination 3
+                print('\n')
+                print('Destination 3 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 3 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '4':
+            # if choice is destination 4
+                print('\n')
+                print('Destination 4 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 4 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '5':
+            # if choice is destination 5
+                print('\n')
+                print('Destination 5 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 5 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '6':
+                break
+
+            else:
+                if dest not in {'1','2','3','4','5','6'}:
+                    print("\n Invalid input, try again")
+                    continue
+
+        elif geo == '3':
+        # if choice is 3)
+            print('\n')
+            print('Europe\'s Top travel destinations')
+            print('Destination 1:')
+            print('Destination 2:')
+            print('Destination 3:')
+            print('Destination 4:')
+            print('Destination 5: \n')
+            print('Enter the number of your selection and press return, or enter 6 to choose a new continent.')
+            dest = input('Selection:')
+            if dest == '1':
+            # if choice is destination 1
+                print('\n')
+                print('Destination 1 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 1 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            if dest == '2':
+            # if choice is destination 2
+                print('\n')
+                print('Destination 2 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 2 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            if dest == '3':
+            # if choice is destination 3
+                print('\n')
+                print('Destination 3 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 3 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            if dest == '4':
+            # if choice is destination 4
+                print('\n')
+                print('Destination 4 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 4 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            if dest == '5':
+            # if choice is destination 5
+                print('\n')
+                print('Destination 5 \n')
+                print('Current weather: ')
+                print('weather example.... \n')
+                print ('Wiki tourism section....')
+                print('example tourism section..... \n')
+                print('Cheapest flight from destination 5 to anywhere')
+                print('flight section example.... \n')
+                print('Top tripadvisor recomendation: ')
+                print('#1) Hotel is...... \n')
+                print('.......................................................')
+
+            elif dest == '6':
+                break
+
+            else:
+                if dest not in {'1','2','3','4','5','6'}:
+                    print("\n Invalid input, try again")
+                    continue
+
+        elif geo =='4':
+            # exit the program
+            # break
+            sys.exit()
+
+        else:
+        # if not valid input, print "invalid input" and go to top
+            if geo not in {'1','2','3','4'}:
+                print("\n Invalid input, try again")
+                break
